f508555d4c200bf99113a8a8d0cbb1ec915e17b6
Netoun <coulonnier.nicolas.44@gmail.com>
2019-02-25 17:15:01 +0100

    add home

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,47 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <img
+          class="logo_toolbar"
+          src="@/assets/logo.png"
+        />
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        flat
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+<script>
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+export default {
+  name: 'App',
+  components: {
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+  },
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
+
+<style scoped>
+.logo_toolbar {
+  width: 50px;
+  margin-top: 10px;
 }
 </style>
+
---
+++ src/components/GetStarted.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-container>
+    <v-layout
+      column
+      align-center
+      justify-center
+      class="white--text layout_start"
+    >
+      <img
+        src="@/assets/logo.png"
+        alt="Vuetify.js"
+        height="250"
+      >
+      <div class="primary--text mb-1 display-1 text-uppercase title-started text-xs-center">Le premier site de compétition de tennis de table agile</div>
+      <div class="primary--text font-weight-thin subheading mb-3 text-xs-center">Powered by CapsuleLab</div>
+      <v-btn
+        class="blue lighten-2 mt-5"
+        dark
+        large
+        href="/pre-made-themes"
+      >
+        Get Started
+      </v-btn>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'GetStarted',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.layout_start {
+  margin-top: 5%;
+  font-family: "Varela Round", sans-serif !important;
+}
+
+.title-started {
+  margin-top: 50px;
+}
+</style>
